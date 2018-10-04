8ecab12eeb27caac37769c936546a1ea0c9a41d8
Lots <lots@mssg.net>
2018-10-04 17:47:36 +0800

    testphp添加第5行dddddd

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 $a = "Original";
 $my_array = array("a" => "Cat", "b" => "Dog", "c" => "Horse");
-
 extract($my_array, EXTR_PREFIX_SAME, "dup");
-
 echo  $a.'<br>';
 $age=array("Bill"=>"60","Steve"=>"56","Mark"=>"31");
 print_r(array_change_key_case($age,CASE_UPPER));
-?>
-<h1>这是一行测试</h1>
