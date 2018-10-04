d0d3557632e709567600f987c6b40626d22cf543
Lots <lots@mssg.net>
2018-10-04 16:38:22 +0800

    添加php数组健变大写

--- index.php
+++ index.php
@@ -4,5 +4,7 @@ $my_array = array("a" => "Cat", "b" => "Dog", "c" => "Horse");
 
 extract($my_array, EXTR_PREFIX_SAME, "dup");
 
-echo  $a;
+echo  $a.'<br>';
+$age=array("Bill"=>"60","Steve"=>"56","Mark"=>"31");
+print_r(array_change_key_case($age,CASE_UPPER));
 ?>
